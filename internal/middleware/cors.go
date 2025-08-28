e248ad8ebe6cdb295f5706b68aae10e33ba97571
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2025-08-28 18:44:02 +0300

    feat: update cors policy

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,10 +1,25 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"slices"
+)
 
 func CORS(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		allowedOrigins := []string{
+			"http://localhost:3000",
+			"http://radio.local:3000",
+		}
+
+		origin := r.Header.Get("Origin")
+
+		isAllowed := slices.Contains(allowedOrigins, origin)
+
+		if isAllowed {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "QUERY, GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
